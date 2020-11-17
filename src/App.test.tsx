bfcbdebffe50626c55b873d1760f08d93b303682
unnamed <>
2020-11-17 11:42:36 +0100

    Cleaned CRA files.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app with app name displayed', () => {
+	render(<App />);
+	const appName = screen.getByText(/IP Address Tracker/i);
+	expect(appName).toBeInTheDocument();
 });
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+	interface ProcessEnv {
+		NODE_END: 'development' | 'production' | 'test';
+		PUBLIC_URL: string;
+		REACT_APP_API_URL: string;
+		PORT: number;
+		GENERATE_SOURCEMAP: boolean;
+	}
+}
